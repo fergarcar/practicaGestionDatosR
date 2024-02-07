43224abbcbf85a9ce8c4270f53c7d6ad196bcc4f
fergarcar <carmen26garcia@gmail.com>
2024-02-07 12:44:14 +0100

    Create practicaGestionDatosR.R

---
+++ practicaGestionDatosR.R
@@ -0,0 +1,27 @@
+getwd()
+nuevo_dir <-"C:/practicaGestionDatosR"
+setwd (nuevo_dir)
+
+#1
+
+vector <-c(1,2,3,4,5,6)
+
+resultado_comp_vectores <-6%in%vector
+print(resultado_comp_vectores)
+
+edades_descubrimientos <-c(1868, 1922, 1974, 1812, 1974, 1911)
+
+#2
+vector <-c(1,2,3,4,5,6)
+
+resultado_comp_vectores <-6%in%vector
+print(resultado_comp_vectores)
+
+cantidad_artefactos <-c(1, 5398, 8000, 49, 52, 40000)
+
+#3
+vector <-c(1,2,3,4,5,6)
+resultado_comp_vectores <-6%in%vector
+print (resultado_comp_vectores)
+
+profundidad_hallazgos <-c()
